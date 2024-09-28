26e3f6e7179e5cba5f887f5273229b4753d80671
Robert Kazirut <robert.kazirut@gmail.com>
2024-09-28 17:48:33 -0400

    formatting

--- sql/migrations/V003_addPublicBoolToCharDefaultTrue.sql
+++ sql/migrations/V003_addPublicBoolToCharDefaultTrue.sql
@@ -1,12 +1,13 @@
-DO $$
-DECLARE
-table_name TEXT;
-BEGIN
-FOR table_name IN
-SELECT tablename
-FROM pg_tables
-WHERE tablename LIKE '%_characters'
-LOOP
-EXECUTE 'ALTER TABLE ' || table_name || ' ADD COLUMN is_public boolean DEFAULT true;';
+DO $ $ DECLARE table_name TEXT;
+
+BEGIN FOR table_name IN
+SELECT
+    tablename
+FROM
+    pg_tables
+WHERE
+    tablename LIKE '%_characters' LOOP EXECUTE 'ALTER TABLE ' || table_name || ' ADD COLUMN is_public boolean DEFAULT true;';
+
 END LOOP;
-END $$;
+
+END $ $;
